b284ce49e42b32e9b097e053f703a13b9faf6d04
valeriannnne <valerie.liann@gmail.com>
2024-10-09 14:54:18 +0800

    confirmation alert

--- src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
@@ -6,13 +6,24 @@ import part3_4.com.demoqa.base.BaseTest;
 
 import static utilities.SwitchToUtility.*;
 
+@Test
+
 public class AlertsTest extends BaseTest {
-    @Test
+
     public void testInformationAlert(){
         String expectedAlertText = "You clicked a button";
         var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
         alertsPage.clickInformationAlertButton();
         Assert.assertEquals(getAlertText(), expectedAlertText, "\n Actual & Expected Messages Do Not Match\n");
         acceptAlert();
+    }
+
+    public void testConfirmationAlert(){
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickConfirmationAlertButton();
+        dismissAlert();
+        String actualConfirmationResult = alertsPage.getConfirmationResult();
+        String expectedConfirmationResult = "You selected Cancel";
+        Assert.assertEquals(actualConfirmationResult, expectedConfirmationResult, "\n You Did Not Select Cancel \n");
     }
 }
